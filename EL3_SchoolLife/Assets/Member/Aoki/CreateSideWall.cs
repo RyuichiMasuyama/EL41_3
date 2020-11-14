59c72f1ceb92645a4bf042b82cb0aecf7661e5b0
junichi <tvpcmen@gmail.com>
2020-11-14 16:27:11 +0900

    a

---
+++ EL3_SchoolLife/Assets/Member/Aoki/CreateSideWall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSideWall : MonoBehaviour
+{
+    [SerializeField] private GameObject Wall;
+    [SerializeField] private int CreateCount;
+    [SerializeField] private float Between;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 originPos = transform.position;
+        for (int i = 1; i < CreateCount + 1; i++) {
+            originPos.z += Between * i;
+            Instantiate(Wall, originPos, gameObject.transform.rotation);
+        }
+    }
+}
---
+++ EL3_SchoolLife/Assets/Member/Aoki/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text ScoreCountUI;
+    public int score;
+    [SerializeField] private Text TimeCountUI;
+    public int time;
+
+    private float OneSecondTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreCountUI.text = score.ToString();
+        TimeCountUI.text = time.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreUpdate();
+        TimeUpdate();
+    }
+
+    private void ScoreUpdate() {
+        ScoreCountUI.text = score.ToString();
+    }
+
+    private void TimeUpdate() {
+        OneSecondTime += Time.deltaTime;
+        if(OneSecondTime >= 1.0f) {
+            OneSecondTime -= 1.0f;
+            time--;
+        }
+        TimeCountUI.text = time.ToString();
+    }
+}
