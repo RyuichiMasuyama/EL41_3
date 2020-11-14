86dff93405d30538bcc723a331b5cbd70530c690
junichi <tvpcmen@gmail.com>
2020-11-14 16:23:23 +0900

    LastCommit

--- EL3_SchoolLife/Assets/Player/Script/InpactManager.cs
+++ EL3_SchoolLife/Assets/Player/Script/InpactManager.cs
@@ -7,17 +7,19 @@ namespace nInpactManager
     {
         [SerializeField] public float inpacet;
 
+        Controller controller;
 
         // Start is called before the first frame update
         void Start()
         {
+            controller = GameObject.Find("Controller").GetComponent<Controller>();
 
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            inpacet = controller.Data[0] + controller.Data[1];
         }
     }
 
